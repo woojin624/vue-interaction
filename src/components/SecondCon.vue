09092d7221d5799ee1fe16d10f94b3b3c52e6a7f
woojin624 <54699702+woojin624@users.noreply.github.com>
2021-04-27 17:17:02 +0900

    testing splide-vue(carousel slider) almost done. but still testing how to play apng once

--- src/components/SecondCon.vue
+++ src/components/SecondCon.vue
@@ -2,28 +2,71 @@
   <div class="Second-con">
     <h2 class="sec-title">Friends</h2>
     <div class="slide-container">
-      <div class="sliders">
-        <div class="slide s1">
-          <img class="slide-img" src="../assets/apngtest2.png" alt="코끼리apng" />
-        </div>
-        <div class="slide s2">
-          <img class="slide-img" src="../assets/apngtest2.png" alt="코끼리apng" />
-        </div>
-        <div class="slide s3">
-          <img class="slide-img" src="../assets/apngtest2.png" alt="코끼리apng" />
-        </div>
+      <splide :slides="slides" :options="options" @splide:move="onMoved">
+        <splide-slide class="slide-wrap" v-for="slide in slides" :key="slide.index" :style="{ backgroundColor: slide.back }">
+          <h2 class="slide-char-name">{{ slide.name }}</h2>
+          <img class="slide-img" :src="slide.src" />
+        </splide-slide>
+      </splide>
+      <div class="char-desc-wrap">
+        <div class="char-desc" v-if="this.currentIdx === 0">라이언 페이지</div>
+        <div class="char-desc" v-if="this.currentIdx === 1">어피치 페이지</div>
+        <div class="char-desc" v-if="this.currentIdx === 2">프로도 페이지</div>
+        <div class="char-desc" v-if="this.currentIdx === 3">네오 페이지</div>
+        <div class="char-desc" v-if="this.currentIdx === 4">무지 페이지</div>
       </div>
-      <div class="slide-button left-btn"></div>
-      <div class="slide-button right-btn"></div>
     </div>
   </div>
 </template>
 
 <script>
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+import elepant from '../assets/apngtest2.png';
+import whatisthis from '../assets/apngtest1.png';
+
 export default {
   name: 'SecondCon',
-
-  data() {},
+  components: {
+    Splide,
+    SplideSlide,
+  },
+  data() {
+    return {
+      slides: [
+        { src: elepant, index: 0, name: 'ryan', back: 'rgb(240, 176, 38)' },
+        { src: whatisthis, index: 1, name: 'apeach', back: 'rgb(240, 157, 179)' },
+        { src: elepant, index: 2, name: 'prodo', back: 'rgb(79, 135, 238)' },
+        { src: whatisthis, index: 3, name: 'neo', back: 'rgb(184, 111, 47)' },
+        { src: elepant, index: 4, name: 'muzi', back: 'rgb(181, 155, 69)' },
+      ],
+      options: {
+        type: 'loop',
+        // rewind: true,
+        focus: 'center',
+        trimSpace: false,
+        width: '100%',
+        // perPage: 1.7,
+        gap: '1rem',
+        padding: {
+          left: '2rem',
+          right: '2rem',
+        },
+        pagination: false,
+        arrows: false,
+      },
+      currentIdx: 0,
+    };
+  },
+  methods: {
+    onMoved(root, newIndex) {
+      //   console.log('root : ', root);
+      //   console.log('newIndex : ', newIndex);
+      //   console.log('oldIndex : ', oldIndex);
+      this.currentIdx = newIndex;
+      console.log('this.currentIdx : ', this.currentIdx);
+    },
+  },
 };
 </script>
 
@@ -31,7 +74,6 @@ export default {
 .Second-con {
   width: 100%;
   height: auto;
-  padding: 0 20px;
 }
 .sec-title {
   font-size: 48px;
@@ -42,42 +84,37 @@ export default {
   position: relative;
   height: 500px;
 }
-.sliders {
-  width: 300%;
-  position: absolute;
-  display: flex;
-}
-
-.slide {
-  width: 30%;
-  height: 500px;
-  background: rgb(237, 186, 124);
-  border-radius: 30px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 12px;
-  margin-bottom: 96px;
-}
 
 .slide-img {
   width: 100%;
+  height: auto;
+}
+.slide-wrap {
+  border-radius: 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+.slide-char-name {
+  font-size: 32px;
+  margin-top: 32px;
 }
 
-.slide-button {
-  width: 30px;
-  height: 30px;
-  background: tomato;
+.char-desc-wrap {
+  width: 100%;
+  margin: 48px 0;
+  padding: 0 20px;
+  height: auto;
 }
 
-.left-btn {
-  position: absolute;
-  top: 50%;
-  left: 0;
-}
-.right-btn {
-  position: absolute;
-  top: 50%;
-  right: 0;
+.char-desc {
+  padding: 20px;
+  text-align: center;
+  font-size: 36px;
+  font-weight: 700;
+  width: 100%;
+  background: rgb(201, 201, 201);
+  border-radius: 30px;
 }
 </style>
